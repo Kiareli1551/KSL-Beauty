7fd76f8c46f9783eea98ee4cb98e9c0957490287
Lolita Calva <lolitacalva1@gmail.com>
2025-12-03 22:10:07 -0800

    Create conexion.php

---
+++ ksl-backend/conexion.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json; charset=utf-8");
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "ksl_beauty";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+?>
