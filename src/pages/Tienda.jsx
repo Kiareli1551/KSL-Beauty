1809eab4399988641568d0b8a6495e9dfed0ddd7
Lolita Calva <lolitacalva1@gmail.com>
2025-12-03 22:23:08 -0800

    Add files via upload

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -5,25 +5,44 @@ export default function Tienda() {
   const [productos, setProductos] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const [pagina, setPagina] = useState(1);
+  const [totalPaginas, setTotalPaginas] = useState(1);
+
   useEffect(() => {
-    fetch("https://dummyjson.com/products?limit=10")
+    setLoading(true);
+    fetch(`http://localhost/ksl-backend/producto/obtenerProductos.php?pagina=${pagina}`)
       .then((res) => res.json())
       .then((data) => {
-        setProductos(data.products);
+        setProductos(data.productos);
+        setTotalPaginas(Math.ceil(data.total / data.porPagina));
         setLoading(false);
       });
-  }, []);
+  }, [pagina]);
 
   if (loading) return <p className="text-center mt-5">Cargando productos...</p>;
 
   return (
-    <div className="tienda-grid">
-      <div className="container mt-4">
-        <div className="row row-cols-1 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-          {productos.map((p) => (
-            <ProductosCard key={p.id} producto={p} />
-          ))}
-        </div>
+    <div className="container mt-4">
+      {/* Productos */}
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+        {productos.map((p) => (<ProductosCard key={p.idProducto} producto={p} />))}
+      </div>
+
+      {/* Paginador */}
+      <div className="d-flex justify-content-center mt-4 mb-5">
+        <button className="btn btn-outline-dark mx-2"
+          disabled={pagina === 1}
+          onClick={() => setPagina(pagina - 1)}
+        >&laquo; Anterior
+        </button>
+
+        <span className="px-3 py-2">Página {pagina} de {totalPaginas}</span>
+
+        <button className="btn btn-outline-dark mx-2"
+          disabled={pagina === totalPaginas}
+          onClick={() => setPagina(pagina + 1)}
+        >Siguiente &raquo;</button>
+
       </div>
     </div>
   );
