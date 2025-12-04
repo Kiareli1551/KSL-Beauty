c4a28965b22cc2b82ed204120810ed8d0a480d65
Lolita Calva <lolitacalva1@gmail.com>
2025-12-03 22:20:24 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Inicio from "./pages/Inicio";
 import Tienda from "./pages/Tienda";
 import Nosotras from "./pages/Nosotras";
 import Contacto from "./pages/Contacto";
+import Usuario from "./pages/Usuario";
+import AdminPanel from "./components/AdminPanel";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
       <Routes>
@@ -15,7 +18,31 @@ export default function App() {
         <Route path="/tienda" element={<Tienda />} />
         <Route path="/nosotras" element={<Nosotras />} />
         <Route path="/contacto" element={<Contacto />} />
+
+        {/* Página que controla login / perfil / admin */}
+        <Route path="/usuario" element={<Usuario />} />
+
+        {/* Área del cliente */}
+        <Route
+          path="/perfil"
+          element={
+            <ProtectedRoute allowedRoles={["cliente", "administrador"]}>
+              <h2>Perfil del Usuario</h2>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Área del administrador */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["administrador"]}>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
